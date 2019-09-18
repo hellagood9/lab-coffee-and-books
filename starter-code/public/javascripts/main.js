3729d26ddf911c3f6560dc8f85287ec5a901e82b
Maximiliano Sussini <maxsus60@gmail.com>
2019-09-18 07:12:57 +0200

    styling the map

--- starter-code/public/javascripts/main.js
+++ starter-code/public/javascripts/main.js
@@ -1,27 +1,194 @@
 window.onload = () => {
-  const madrid = {
-    lat: 40.4165000,
-    lng: -3.7025600,
+  const NY = {
+    lat: 40.73061,
+    lng: -73.935242
   };
 
   const markers = [];
 
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
-    center: madrid,
-  });
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: NY,
+    styles: [
+      {
+          "featureType": "all",
+          "elementType": "labels.text.fill",
+          "stylers": [
+              {
+                  "saturation": 36
+              },
+              {
+                  "color": "#000000"
+              },
+              {
+                  "lightness": 40
+              }
+          ]
+      },
+      {
+          "featureType": "all",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+              {
+                  "visibility": "on"
+              },
+              {
+                  "color": "#000000"
+              },
+              {
+                  "lightness": 16
+              }
+          ]
+      },
+      {
+          "featureType": "all",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "administrative",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#000000"
+              },
+              {
+                  "lightness": 20
+              }
+          ]
+      },
+      {
+          "featureType": "administrative",
+          "elementType": "geometry.stroke",
+          "stylers": [
+              {
+                  "color": "#000000"
+              },
+              {
+                  "lightness": 17
+              },
+              {
+                  "weight": 1.2
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "color": "#000000"
+              },
+              {
+                  "lightness": 20
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "color": "#000000"
+              },
+              {
+                  "lightness": 21
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#000000"
+              },
+              {
+                  "lightness": 17
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [
+              {
+                  "color": "#000000"
+              },
+              {
+                  "lightness": 29
+              },
+              {
+                  "weight": 0.2
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "color": "#000000"
+              },
+              {
+                  "lightness": 18
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "color": "#000000"
+              },
+              {
+                  "lightness": 16
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "color": "#000000"
+              },
+              {
+                  "lightness": 19
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "color": "#000000"
+              },
+              {
+                  "lightness": 17
+              }
+          ]
+      }
+    ]
+  })
 
-  axios.get('/map')
-    .then(({ data }) => {
-      data.forEach((place) => {
-        new google.maps.Marker({
-          position: {
-            lat: place.location.coordinates[1],
-            lng: place.location.coordinates[0],
-          },
-          title: place.name,
-          map,
-        });
+
+  axios.get("/map").then(({ data }) => {
+    data.forEach(place => {
+      new google.maps.Marker({
+        position: {
+          lat: place.location.coordinates[1],
+          lng: place.location.coordinates[0]
+        },
+        title: place.name,
+        map
       });
     });
+  });
 };
